792e727483151ec755c7be272d9eb505736815e0
Sergio Terroso <sergio.terroso@gmail.com>
2024-03-02 13:18:33 -0600

    Controller-based error handling

---
+++ Controllers/ErrorController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyBGList.Controllers
+{
+    [ApiController]
+    public class ErrorController : ControllerBase
+    {
+        [Route("/error")]
+        [HttpGet]
+        public IActionResult Error()
+        {
+            return Problem();
+        }
+    }
+}
