1f209d26245d70fe1ee13e8a3c432bb3ddc8f2b2
Sergio Terroso <sergio.terroso@gmail.com>
2024-03-02 22:33:36 -0600

    Exercises for chapter 2 done.

--- BoardGame.cs
+++ BoardGame.cs
@@ -1,9 +1,13 @@
 namespace MyBGList;
 public class BoardGame
 {
-    public int Id { get; set; }
+  public int Id { get; set; }
 
-    public string? Name { get; set; }
+  public string? Name { get; set; }
 
-    public int? Year { get; set; }
+  public int? Year { get; set; }
+
+  public int? MinPlayers { get; set; }
+
+  public int? MaxPlayers { get; set; }
 }
--- Controllers/BoardGamesController.cs
+++ Controllers/BoardGamesController.cs
@@ -7,31 +7,37 @@ namespace MyBGList.Controllers;
 [ApiController]
 public class BoardGamesController : ControllerBase
 {
-    private readonly ILogger<BoardGamesController> _logger;
+  private readonly ILogger<BoardGamesController> _logger;
 
-    public BoardGamesController(ILogger<BoardGamesController> logger) {
-        _logger = logger;
-    }
+  public BoardGamesController(ILogger<BoardGamesController> logger) {
+    _logger = logger;
+  }
 
-    [HttpGet(Name="GetBoardGames")]
-    public IEnumerable<BoardGame> Get()
-    {
-        return new[] {
-            new BoardGame() {
-                Id = 1,
-                Name = "Axis and Allies",
-                Year = 1981
-            },
-            new BoardGame() {
-                Id = 2,
-                Name = "Citadels",
-                Year = 2000
-            },
-            new BoardGame() {
-                Id = 3,
-                Name = "Terraforming Mars",
-                Year = 2016
-            }
-        };
-    }
+  [HttpGet(Name="GetBoardGames")]
+  public IEnumerable<BoardGame> Get()
+  {
+    return new[] {
+      new BoardGame() {
+        Id = 1,
+        Name = "Axis and Allies",
+        Year = 1981,
+        MinPlayers = 2,
+        MaxPlayers = 5
+      },
+      new BoardGame() {
+        Id = 2,
+        Name = "Citadels",
+        Year = 2000,
+        MinPlayers = 2,
+        MaxPlayers = 8
+      },
+      new BoardGame() {
+        Id = 3,
+        Name = "Terraforming Mars",
+        Year = 2016,
+        MinPlayers = 1,
+        MaxPlayers = 5
+      }
+    };
+  }
 }
